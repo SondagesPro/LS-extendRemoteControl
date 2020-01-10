d6a6c5ee049354216242e3fd0ec0bcc661015ef5
Denis Chenu <denis@sondages.pro>
2020-01-10 08:39:16 +0100

    [feature] 4.X version compatibility

--- extendRemoteControl.php
+++ extendRemoteControl.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2015-2019 Denis Chenu <http://sondages.pro>
  * @license GPL v3
- * @version 1.1.3
+ * @version 1.2.0
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
